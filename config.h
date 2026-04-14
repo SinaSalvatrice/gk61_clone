87b9489448100bdceb8ebd6beaf0a7dec25ab144
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-14 19:28:39 +0200

    Update config.h

--- config.h
+++ config.h
@@ -2,12 +2,6 @@
 #define CONFIG_H
 
 
-// Deprecated fields removed; see info.json for USB and manufacturer info
-
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 13
-#define MATRIX_ROW_PINS { 2, 3, 4, 5, 6 }
-#define MATRIX_COL_PINS { 7, 8, 9, 10, 11, 12, 13, 14, 15, 26, 27, 28, 29 }
-#define DIODE_DIRECTION ROW2COL
+// Hardware metadata lives in keyboard.json.
 
 #endif // CONFIG_H
