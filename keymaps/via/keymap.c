4d54e58d3ade771f8dce99d82dd3e6777cb617cd
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-20 01:35:48 +0200

    Update keymap.c

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -15,23 +15,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_60_custom(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-          KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_NO,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_SLSH,
-          KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, FN,      KC_APP,  KC_RCTL
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_NO,
+        KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_SLSH, KC_NO,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   KC_NO,   KC_SPC,  KC_NO,   KC_NO,   KC_NO,   KC_RALT, FN,      KC_APP,  KC_NO,   KC_RCTL
     ),
 
     [_FN] = LAYOUT_60_custom(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, RM_TOGG, RM_NEXT, RM_PREV, _______, _______,
-          _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, RM_HUEU, RM_SATU, RM_VALU, _______, KC_NO,
-        _______,          _______, _______, _______, _______, _______, _______, _______, RM_HUED, RM_SATD, RM_VALD,          _______,
-          _______, _______, _______,                            QK_LOCK,                            RGB,     _______, CAD,     _______
-        ),
-        [_RGB] = LAYOUT_60_custom(
-          _______, RGB_TOG, RGB_MOD, RGB_RMOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-          _______, RGB_HUI, RGB_SAI, RGB_VAI,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-          _______, RGB_HUD, RGB_SAD, RGB_VAD,  _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-          _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-          _______, _______, _______,                            _______,                            _______, _______, _______, _______
-    )
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, RM_HUEU, RM_SATU, RM_VALU, _______, KC_NO,
+        _______, KC_NO,   _______, _______, _______, _______, _______, _______, _______, RM_HUED, RM_SATD, RM_VALD, _______, KC_NO,
+        _______, _______, _______, KC_NO,   KC_NO,   QK_LOCK, KC_NO,   KC_NO,   KC_NO,   RGB,     _______, CAD,     KC_NO,   _______
+    ),
+      [_RGB] = LAYOUT_60_custom(
+        _______, RGB_TOG, RGB_MOD, RGB_RMOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_HUI, RGB_SAI, RGB_VAI,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_HUD, RGB_SAD, RGB_VAD,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO,
+        _______, KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NO,
+        _______, _______, _______, KC_NO,   KC_NO,   _______, KC_NO,   KC_NO,   KC_NO,   _______, _______, _______, KC_NO,   _______
+      )
 };
