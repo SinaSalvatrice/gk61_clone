3764c543bc623d93937a6fac1ffec51f6c5ab5d6
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-20 23:09:26 +0200

    fuck that

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -14,25 +14,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_5x14(
         /* Row 0 */ KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         /* Row 1 */ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        /* Row 2 */ KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_NO,
-        /* Row 3 */ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_NO,   KC_RSFT,
-        /* Row 4 */ KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   KC_NO,   KC_SPC,  KC_NO,   KC_NO,   KC_NO,   KC_RALT, MO(_FN), KC_MENU, C(L(KC_L)), KC_RCTL
+        /* Row 2 */ KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        /* Row 3 */ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        /* Row 4 */ KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_RALT, MO(_FN), KC_MENU, C(L(KC_L))
     ),
 
     [_FN] = LAYOUT_5x14(
         /* Row 0 */ KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         /* Row 1 */ KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,   KC_NO,
-        /* Row 2 */ KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_ENT,  KC_NO,
-        /* Row 3 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL,  KC_END,  KC_NO,   KC_NO,   KC_PGDN,
-        /* Row 4 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MO(_FN), KC_NO,   KC_NO,   KC_NO
+        /* Row 2 */ KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_ENT,
+        /* Row 3 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL,  KC_END,  KC_NO,
+        /* Row 4 */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MO(_FN), KC_NO
     ),
 
     [_RGB] = LAYOUT_5x14(
         /* Row 0 */ _______, RM_TOGG, RM_NEXT, RM_PREV, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         /* Row 1 */ _______, RM_HUEU, RM_SATU, RM_VALU,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        /* Row 2 */ _______, RM_HUED, RM_SATD, RM_VALD,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        /* Row 3 */ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        /* Row 4 */ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        /* Row 2 */ _______, RM_HUED, RM_SATD, RM_VALD,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        /* Row 3 */ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        /* Row 4 */ _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
 
