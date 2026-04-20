f4ac323e27515fc8036da7bab30b30ce234e64bf
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-20 15:42:01 +0200

    Update keyboard.h

--- keyboard.h
+++ keyboard.h
@@ -2,19 +2,3 @@
 
 #include "quantum.h"
 
-// Custom layout macro to match your keymap arrangement (14 keys per row for the first three rows)
-#define LAYOUT_60_custom( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
-	K40, K41, K42, K43, K44, K45, K46, K47 \
-) \
-{ \
-	{ K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D }, \
-	{ K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D }, \
-	{ K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D }, \
-	{ K30, KC_NO, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, KC_NO }, \
-	{ K40, K41, K42, KC_NO, KC_NO, K43, KC_NO, KC_NO, KC_NO, K44, K45, K46, KC_NO, K47 } \
-}
-
