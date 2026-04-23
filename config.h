29582662899c7eba4f46a7093f469bf93bb783ee
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-24 00:44:11 +0200

    Update config.h

--- config.h
+++ config.h
@@ -8,4 +8,7 @@
 #define MATRIX_COL_PINS { GP5, GP6, GP7, GP8, GP9, GP10, GP11, GP12, GP13, GP14, GP15, GP26, GP27, GP28 }
 #define DIODE_DIRECTION COL2ROW
 
+// VIA requires a unique keyboard UID for detection
+#define VIA_KEYBOARD_UID { 0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x10, 0x20 }
+
 #endif // CONFIG_H
